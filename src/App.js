4773fe43e07be45d57715aefcf260ae3e28728d9
Shiva Gunasehkaran <g.sivakumar@infogain.com>
2020-04-28 01:28:39 +0800

    "App" folder moved

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>{'sdffgfd'}</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
